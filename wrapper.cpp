8265c6c919076a751746022f1aaef76c739c343d
Axel Demborg <axel.demborg@gmail.com>
2022-02-26 14:24:00 +0100

    first step towards diy qr detector - thresholding

--- wrapper.cpp
+++ wrapper.cpp
@@ -4,19 +4,41 @@
 #include <emscripten/bind.h>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core/mat.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
-void gray(int tmp, const int height, const int width)
+struct Parameter {
+    int value;
+    int max;
+    std::string name;
+};
+
+struct Parameters {
+    Parameter threshold = {200, 255, "threshold"};
+};
+
+void detect(int tmp, const int height, const int width, const Parameters parameters)
 {
     auto io_array = reinterpret_cast<uint8_t*>(tmp);
     cv::Mat img(height, width, CV_8UC4, io_array);
     cv::Mat1b input(height, width);
     cv::cvtColor(img, input, cv::COLOR_RGBA2GRAY);
+    cv::threshold(input, input, parameters.threshold.value, 255, cv::THRESH_BINARY);
     cv::cvtColor(input, img, cv::COLOR_GRAY2RGBA);
 }
 
+Parameters getParameters()
+{
+    return Parameters();
+}
+
 EMSCRIPTEN_BINDINGS(wrapper) {
-    emscripten::function("gray", &gray, emscripten::allow_raw_pointers());
+    emscripten::value_object<Parameter>("Parameter")
+        .field("value", &Parameter::value)
+        .field("max", &Parameter::max)
+        .field("name", &Parameter::name);
+    emscripten::value_array<Parameters>("Parameters")
+        .element(&Parameters::threshold);
+    emscripten::function("detect", &detect, emscripten::allow_raw_pointers());
+    emscripten::function("getParameters", &getParameters);
 }
